d545742437cd538c2e90c9985269733b03d045ad
Abhijeet <72185317+Zeref-XXX@users.noreply.github.com>
2026-07-09 19:42:40 +0530

    Time: 38 ms (17.2%), Space: 54.9 MB (50.05%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -4,14 +4,12 @@
  * @return {number[]}
  */
 var twoSum = function (nums, target) {
-    let mpp = new Map();
-    // for(let num of nums){
-    for (let i = 0; i < nums.length; i++) {
-        let temp = target - nums[i]
-        if (mpp.has(temp)) {
-            console.log(mpp.get(temp), i)
-            return [mpp.get(temp), i]
+    for (let i = 0; i < nums.length - 1; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if ((nums[i] + nums[j]) == target) {
+                console.log(i, j)
+                return [i, j];
+            }
         }
-        mpp.set(nums[i], i)
     }
 };
