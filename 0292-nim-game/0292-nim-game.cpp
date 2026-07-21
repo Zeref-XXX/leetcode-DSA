7332fbbec0bd959964f33486581d1dde3b4354dc
48h1337 <72185317+Zeref-XXX@users.noreply.github.com>
2026-07-21 17:30:39 +0530

    Time: 0 ms (100%), Space: 7.8 MB (16.86%) - LeetHub

--- 0292-nim-game/0292-nim-game.cpp
+++ 0292-nim-game/0292-nim-game.cpp
@@ -2,14 +2,9 @@ class Solution {
 public:
     bool canWinNim(int n) {
 
-        if (n <= 3) {
+        if (n % 4 == 0)
+            return false;
+        else
             return true;
-        } 
-        int rem=n%4;
-        if(n%4==0) return false;
-        else return true;
-
-       
-
     }
 };
