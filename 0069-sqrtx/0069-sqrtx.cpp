b8d3642f0fc23e1ea3f5d9c07c8dfb4b541bf723
48h1337 <72185317+Zeref-XXX@users.noreply.github.com>
2026-07-23 12:01:24 +0530

    Time: 60 ms (6.5%), Space: 8.6 MB (14.62%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -1,13 +1,19 @@
 class Solution {
 public:
     int mySqrt(int x) {
+        int start = 1, end = x;
         int ans = 1;
-        if(x<1) return 0;
-        for (int i = 2; i <= x / i; i++) {
-             
-            if (i * i <= x)
-                ans = i;
-        }
-        return ans;
+        while (start <= end) {
+            int mid =start+(end-start)/ 2;
+            cout<<mid<<" ";
+            if(mid<=x/mid){
+                ans=mid;
+                start=mid+1;
+            }
+            else{
+                end=mid-1;
+            }
+        }   
+        return x == 0 ? 0 : ans;
     }
 };
